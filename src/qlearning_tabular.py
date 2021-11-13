5e1cab96d46b3018edc4218ec5534857cd70af86
elianlaura <elian.laura.riv@gmail.com>
2021-11-13 11:57:08 -0300

    Update qlearning_tabular.py

--- src/qlearning_tabular.py
+++ src/qlearning_tabular.py
@@ -1,116 +1,206 @@
-from sklearn.model_selection import ParameterGrid
-from datetime import datetime 
-import numpy as np
-import pandas as pd
 import gym
+import numpy as np
+import sys
+import os
+import time
+import pandas
+import random
+import pickle
+import itertools
+from collections import defaultdict
+import matplotlib.pyplot as plt
+from sklearn.model_selection import ParameterGrid
 
-env = gym.make("FrozenLake-v0", is_slippery=False, map_name='8x8')
-n_observations = env.observation_space.n
-n_actions = env.action_space.n
+
+#from utils import plotting
+# Deterministic environment FrozenLake-v0
+def print_state_values_q(Q, size=8):
+    print("\n\t\t\t State Value")
+
+    print("--------------------------------------------------------")
+    for i in range(size):
+        for j in range(size):
+            k = i*size + j
+            q = np.max(Q[k])
+            if q >= 0:
+                print("  %.2f|" % np.max(Q[k]), end="")
+            else:
+                print(" %.2f|" % np.max(Q[k]), end="")
+        print("\n--------------------------------------------------------")
+
+def print_policy_q(Q, size=8):
+
+    actions_names = ['\u2190', '\u2193', '\u2192', '\u2191']
+
+    print("\n\t\t Policy/Actions")
+
+    print("------------------------------------------------")
+    for i in range(size):
+        for j in range(size):
+            k = i*size + j
+            q = np.argmax(Q[k])
+            print("  %s  |" % actions_names[q], end="")
+        print("\n------------------------------------------------")
+
 
 #Initialize the Q-table to 0
-Q_table = np.zeros((n_observations,n_actions))
-#print(Q_table)
+def qlearning_tabular(env, episodes=10000, epsilon_max=1.0, epsilon_decay=0.001, min_epsilon=0.01, gamma_=0.99, alpha=0.1 ):
+    n_observations = env.observation_space.n
+    n_actions = env.action_space.n
 
-#number of episode we will run
-n_episodes = 50000
+    Q_table = np.zeros((n_observations,n_actions))
+    #number of episode we will run
+    n_episodes = episodes
 
-#maximum of iteration per episode
-max_iter_episode = 1000
+    #maximum of iteration per episode
+    max_iter_episode = 100
 
-#discounted factor
-gamma = 0.95
+    #initialize the exploration probability to 1
+    exploration_proba = epsilon_max
 
-#learning rate
-lr = 0.001
+    #exploartion decreasing decay for exponential decreasing
+    exploration_decreasing_decay = epsilon_decay
 
-#initialize the exploration probability to 1
-epsilon = 1 #exploration_proba
+    # minimum of exploration proba
+    min_exploration_proba = min_epsilon
 
-#exploartion decreasing decay for exponential decreasing
-exploration_decreasing_decay = 0.001
+    #discounted factor
+    gamma = gamma_
 
-# minimum of exploration proba
-min_exploration_proba = 0.01
+    #learning rate
+    lr = alpha
 
-# Initialize list of rewards
-rewards = np.zeros(n_episodes)
+    # Initialize list of rewards
+    rewards_per_episode = list()
 
-def grid_search_tabular(comb, lr, gamma, epsilon):
-    # #sum the rewards that the agent gets from the environment
-    rewards_per_episode = np.zeros(n_episodes)
-
+    #we iterate over episodes
     for e in range(n_episodes):
         #we initialize the first state of the episode
         current_state = env.reset()
         done = False
 
-        for i in range(max_iter_episode): 
+        #sum the rewards that the agent gets from the environment
+        total_episode_reward = 0
+
+        for t in itertools.count():
             # we sample a float from a uniform distribution over 0 and 1
-            # if the sampled float is less than the exploration proba
+            # if the sampled flaot is less than the exploration proba
             #     the agent selects arandom action
             # else
-            #     he exploits his knowledge using the bellman equation 
-            
-            if np.random.uniform(0,1) < epsilon:
-                action = env.action_space.sample()
+            #     he exploits his knowledge using the bellman equation
+
+            if np.random.uniform(0,1) < exploration_proba:
+                action = np.random.choice(env.action_space.n)
             else:
                 action = np.argmax(Q_table[current_state,:])
-            
+
             # The environment runs the chosen action and returns
             # the next state, a reward and true if the epiosed is ended.
             next_state, reward, done, _ = env.step(action)
-            
+
+            if done and reward == 0:
+                reward = -1.0
+
             # We update our Q-table using the Q-learning iteration
-            Q_table[current_state, action] = (1-lr) * Q_table[current_state, action] +lr*(reward + gamma*max(Q_table[next_state,:]))
-                    
-            rewards_per_episode[e] += reward
+            Q_table[current_state, action] = Q_table[current_state, action] +lr*(reward + gamma*max(Q_table[next_state,:]) - Q_table[current_state, action])
+            total_episode_reward = total_episode_reward + reward
 
             # If the episode is finished, we leave the for loop
             if done:
                 break
+
             current_state = next_state
 
-        
-        #print("Done, iters, total_episode_reward:", done, aux, total_episode_reward) 
+        #We update the exploration proba using exponential decay formula
+        exploration_proba = max(min_exploration_proba, np.exp(-exploration_decreasing_decay*e))
 
-        #We update the exploration proba using exponential decay formula 
-        epsilon = max(min_exploration_proba, np.exp(-exploration_decreasing_decay*e))
-        
-        # Add rewards for this episode
-        #if done==True and rewards_per_episode[e]==0.0:
-            #rewards_per_episode[e] = -1
+        rewards_per_episode.append(total_episode_reward)
 
-    return rewards_per_episode
-    
-param_grid = {'lr': [0.01, 0.001, 0.0001], 'gamma': [0.8, 0.9, 1.0], 'epsilon':[1.0, 0.7, 0.5]}
-table = pd.DataFrame(columns=['c', 'learning_rate', 'gamma', 'epsilon', 'wins', 'elapsed time (s)'])
+    return Q_table, rewards_per_episode
 
-comb = 0
-rewards_per_model = []
-np.random.seed(2)
-env.seed(2)
+def qlearning_tabular_test(env, Q, episodes=100, display=False):
+    Q_table = Q
+    #number of episode we will run
+    n_episodes = episodes
 
-for c in ParameterGrid(param_grid):
-    print("Combination ", comb)
+    #maximum of iteration per episode
+    wins_ = 0
 
-    t0 = datetime.now()
-    rewards = grid_search_tabular(comb, **c)
-    elapsed_time = datetime.now() - t0
-    
-    rewards_per_model.append(rewards)
-    wins = (rewards > 0).sum()
+    #we iterate over episodes
+    for e in range(n_episodes):
+        #we initialize the first state of the episode
+        current_state = env.reset()
+        done = False
 
-    new_row = { 'c' : comb,
-                'learning_rate': c['lr'],
-                'gamma': c['gamma'],
-                'epsilon': c['epsilon'],
-                'wins': wins,
-                'elapsed time (s)': elapsed_time}
+        #sum the rewards that the agent gets from the environment
+        total_episode_reward = 0
 
-    table = table.append(new_row, ignore_index=True) 
-    comb += 1
-    print(new_row) 
+        for t in itertools.count():
+            if display==True:
+                env.render()
+
+            action = np.argmax(Q_table[current_state,:])
+
+            # The environment runs the chosen action and returns
+            # the next state, a reward and true if the epiosed is ended.
+            next_state, reward, done, _ = env.step(action)
+
+            # If the episode is finished, we leave the for loop
+            if done:
+                wins_ += reward
+                if display == True:
+                    env.render()
+
+                break
+
+            current_state = next_state
 
 
-print(table)
+    return wins_/n_episodes
+
+def report_sarsa(stochastic):
+    if stochastic:
+        param_ = {'episodes': [1000, 5000], 'epsilon_decay':[0.01, 0.001, 0.0001], 'alpha':[0.001, 0.01, 0.1], 'gamma':[1.0, 0.99, 0.9, 0.0]}
+    else:
+        param_ = {'episodes': [1000, 5000], 'epsilon_decay':[0.01, 0.001, 0.0001], 'alpha': [0.001, 0.01, 0.1], 'gamma': [1.0, 0.99, 0.9, 0.0]}
+
+    #Environment
+    env = gym.make('FrozenLake8x8-v1', is_slippery=stochastic)
+
+    results = pandas.DataFrame(columns=['episodes', 'epsilon_decay', 'alpha', 'gamma', 'win/loss (%)', 'elapsed time (s)'])
+
+    for c in ParameterGrid(param_):
+        #print(c)
+        # Reset the seed
+        np.random.seed(42)
+        random.seed(42)
+        env.seed(42)
+
+        tic = time.time()
+
+        # Learn policy
+        Q, stats = qlearning_tabular(env, episodes=c['episodes'], epsilon_decay=c['epsilon_decay'], gamma_=c['gamma'], alpha=c['alpha'] )
+
+        toc = time.time()
+
+        elapsed_time = toc - tic
+
+        # Generate wins
+        win = qlearning_tabular_test(env, Q, episodes=100, display=False)*100
+
+        new_row = {'episodes':c['episodes'],
+                   'epsilon_decay':c['epsilon_decay'],
+                   'alpha':   c['alpha'],
+                   'gamma': c['gamma'],
+                   'win/loss (%)': win,
+                   'elapsed time (s)': elapsed_time}
+
+        results = results.append(new_row, ignore_index=True)
+
+    return results
+
+
+if __name__ == '__main__':
+   
+    report = report_sarsa(False)
+    print(report)
